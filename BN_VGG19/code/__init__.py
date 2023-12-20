ccd2da9a3d093442eb7d42a997aa13db74ad1c50
blueeed <120617665+blueeed03@users.noreply.github.com>
2023-12-20 10:49:20 +0800

    Update __init__.py

--- BN_VGG19/code/__init__.py
+++ BN_VGG19/code/__init__.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time     :2021/12/4 17:47
-# @File     :__init__.py.py
-# @Software :PyCharm
-# @Project  :
-# @Content  :
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
