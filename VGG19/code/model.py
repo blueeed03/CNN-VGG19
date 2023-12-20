c7544bd910a50de4900c1051066ed2486805ab50
blueeed <120617665+blueeed03@users.noreply.github.com>
2023-12-20 10:46:01 +0800

    Update model.py

--- VGG19/code/model.py
+++ VGG19/code/model.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time     :2021/12/4 17:47
-# @File     :model.py
-# @Software :PyCharm
-# @Project  :模型
-# @Content  :模型函数
-
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -17,24 +10,29 @@ def model_CNN(num):
         layers.Conv2D(64, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(64, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.MaxPool2D(2, 2),
+        
         layers.Conv2D(128, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(128, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.MaxPool2D(2, 2),
+        
         layers.Conv2D(256, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(256, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(256, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(256, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.MaxPool2D(2, 2),
+        
         layers.Conv2D(512, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(512, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(512, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(512, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.MaxPool2D(2, 2),
+        
         layers.Conv2D(512, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(512, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(512, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.Conv2D(512, 3, strides=(1, 1), padding="same", activation="relu"),
         layers.MaxPool2D(2, 2),
+        
         layers.Flatten(),
         layers.Dense(256, activation="relu"),  # 256
         layers.Dense(128, activation="relu"),
